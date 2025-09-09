1e70c7641b0b08f4575f3c70a6384830b15bcac0
Abrham Tadesse <abrsh5y4@gmail.com>
2025-09-10 00:17:29 +0300

    getting started with agents

---
+++ main.py
@@ -0,0 +1,45 @@
+
+import os
+from functions.get_files_info import get_files_info
+import types 
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+def main():
+    
+    load_dotenv()
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("Error: No prompt provided")
+        sys.exit(1)
+    verbose_flag = False
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+    prompt = sys.argv[1]
+
+    messages = [
+       types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
+    response = client.chat.completions.create(
+        model="gemini-2.0-flash-001",
+        messages=messages
+    )
+
+    print(response.text)
+
+    if response is None or response.usage_metadata is None:
+        print("Error: No response received")
+        sys.exit(1)
+    
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+print(get_files_info("calculator","pkg"))
+# main()
